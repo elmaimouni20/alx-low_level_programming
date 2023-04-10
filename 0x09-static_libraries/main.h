f98aa5d091eb1c544cd73999faf2aa554f9a7dfc
salmaelmaimouni <salmaelmaimouni20@gmail.com>
2023-04-09 17:53:44 -0700

    commit

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);		
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -20,4 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
