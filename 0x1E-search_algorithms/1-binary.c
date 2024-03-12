1c00a520ddc48b1ceb5819e0783f00adc73c2e9f
salmaelmaimouni <salmaelmaimouni20@gmail.com>
2024-03-12 16:55:35 -0700

    fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,61 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * recursive_search - searches for a value in an array of
- * integers using the Binary search algorithm
+ * binary_search - function
+ * @array: desc
+ * @size: desc
+ * @value: desc
  *
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
-int recursive_search(int *array, size_t size, int value)
-{
-	size_t half = size / 2;
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (recursive_search(array, half, value));
-
-	half++;
-
-	return (recursive_search(array + half, size - half, value) + half);
-}
-
-/**
- * binary_search - calls to binary_search to return
- * the index of the number
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * Return: Return
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int index;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t middle;
+	size_t i;
 
-	index = recursive_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
+	if (!array)
 		return (-1);
 
-	return (index);
+	while (left <= right)
+	{
+		middle = left + (right - left) / 2;
+
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+			else
+				printf("\n");
+		}
+
+		if (array[middle] == value)
+			return (middle);
+
+		if (array[middle] < value)
+			left = middle + 1;
+		else
+			right = middle - 1;
+	}
+
+	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,39 +5,39 @@
 #include <stdio.h>
 
 /**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
+ *  * struct listint_s - singly linked list
+ *   *
+ *    * @n: Integer
+ *     * @index: Index of the node in the list
+ *      * @next: Pointer to the next node
+ *       *
+ *        * Description: singly linked list node structure
+ *         * for Holberton project
+ *          */
 typedef struct listint_s
 {
-	int n;
-	size_t index;
-	struct listint_s *next;
+		int n;
+			size_t index;
+				struct listint_s *next;
 } listint_t;
 
 /**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
+ *  * struct skiplist_s - Singly linked list with an express lane
+ *   *
+ *    * @n: Integer
+ *     * @index: Index of the node in the list
+ *      * @next: Pointer to the next node
+ *       * @express: Pointer to the next node in the express lane
+ *        *
+ *         * Description: singly linked list node structure with an express lane
+ *          * for Holberton project
+ *           */
 typedef struct skiplist_s
 {
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
+		int n;
+			size_t index;
+				struct skiplist_s *next;
+					struct skiplist_s *express;
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
